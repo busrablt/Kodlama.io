f7afe71e6581adca6b285c8601374163dd964a09
busrablt <busrablt2121@gmail.com>
2021-02-21 20:07:00 +0300

    MyDictionary

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Customer Information");
+
+
+            MyDictionary<int, string> customerInformation = new MyDictionary<int, string>();
+            
+            customerInformation.Add(1, "Büşra Bulut");
+            customerInformation.Add(2, "Hasret Kalhan");
+            customerInformation.Add(3, "Ece Umuc");
+            customerInformation.Add(4, "Elif Kaya");
+            customerInformation.Add(5, "Selin Vatansever");
+           
+            customerInformation.List();
+            
+            Console.WriteLine("The number of customers");
+            Console.WriteLine(customerInformation.Count);
+
+        }
+
+
+    }
+
+
+}
